dbe3e421a3355eee4e69c2085ed29a8178d64caf
Harryjutt999 <harisjutt090000@gmail.com>
2025-09-15 08:48:33 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-
-  // API routes ke liye runtime force kar diya
-  experimental: {
-    runtime: 'nodejs'
-  }
 };
 
 module.exports = nextConfig;
